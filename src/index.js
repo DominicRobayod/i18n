49dc11f07b3a7139537e788d1c713be872fe64aa
DominicRobayod <dfrobayod@udistrital.edu.co>
2025-05-27 07:41:08 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { IntlProvider } from "react-intl";
 import JobsList from "./components/jobslist";
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+const language = navigator.language.startsWith("es") ? "es" : "en";
+const messages = language === "es" ? localeEsMessages : localeEnMessages;
+
+ReactDOM.render(
+  <IntlProvider locale={language} messages={messages}>
+    <JobsList language={language} />
+  </IntlProvider>,
+  document.getElementById("root")
+);
